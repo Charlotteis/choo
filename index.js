57480f47da3d0b7e5f7b1b6eb731d09974bfca95
Yoshua Wuyts <yoshuawuyts@gmail.com>
2017-04-20 04:06:30 +0200

    become prototype based
    upgrade to new nanobus
    
    remove nanohistory
    
    add nanotiming
    
    remove nanomount
    
    move newTree to next tick
    
    upgrade timing API
    
    add more node warnings to .start/.mount
    
    add morph timing
    
    pass app instance to .use()
    
    self-clearing async-timing
    
    clean up timings
    
    better mount error
    
    fix mount bug
    
    de-prototype helper functions
    
    update to latest nanotiming
    
    upgrade to nanotiming 5
    
    update nanotiming
    
    add toString asserts
    
    more assertions
    
    add event based tracing
    
    expose createlocation

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var documentReady = require('document-ready')
-var nanohistory = require('nanohistory')
+var nanotiming = require('nanotiming')
 var nanorouter = require('nanorouter')
-var nanomount = require('nanomount')
 var nanomorph = require('nanomorph')
 var nanohref = require('nanohref')
 var nanoraf = require('nanoraf')
@@ -11,114 +10,153 @@ var assert = require('assert')
 module.exports = Choo
 
 function Choo (opts) {
+  if (!(this instanceof Choo)) return new Choo(opts)
   opts = opts || {}
+
+  assert.equal(typeof opts, 'object', 'choo: opts should be type object')
 
   var routerOpts = {
     default: opts.defaultRoute || '/404',
     curry: true
   }
 
-  var timingEnabled = opts.timing === undefined ? true : opts.timing
-  var hasWindow = typeof window !== 'undefined'
-  var hasPerformance = hasWindow && window.performance && window.performance.mark
-  var router = nanorouter(routerOpts)
-  var bus = nanobus()
-  var rerender = null
-  var tree = null
-  var state = {}
+  // properties for internal use only
+  this._historyEnabled = opts.history === undefined ? true : opts.history
+  this._hrefEnabled = opts.href === undefined ? true : opts.href
+  this._tree = null
 
-  return {
-    toString: toString,
-    use: register,
-    mount: mount,
-    router: router,
-    route: route,
-    start: start
-  }
+  // properties that are part of the API
+  this.router = nanorouter(routerOpts)
+  this.emitter = nanobus('choo.emit')
+  this.state = {}
+}
 
-  function route (route, handler) {
-    router.on(route, function (params) {
-      return function () {
-        state.params = params
-        return handler(state, emit)
-      }
-    })
-  }
+Choo.prototype.route = function (route, handler) {
+  assert.equal(typeof route, 'string', 'choo.route: route should be type string')
+  assert.equal(typeof handler, 'function', 'choo.handler: route should be type function')
 
-  function register (cb) {
-    cb(state, bus)
-  }
-
-  function start () {
-    if (opts.history !== false) {
-      nanohistory(function (href) {
-        bus.emit('pushState')
+  var self = this
+  this.router.on(route, function (params) {
+    return function () {
+      self.state.params = params
+      var routeTiming = nanotiming("choo.route('" + route + "')")
+      var res = handler(self.state, function (eventName, data) {
+        self.emitter.emit(eventName, data)
       })
+      routeTiming(self._trace.bind(self))
+      return res
+    }
+  })
+}
 
-      bus.prependListener('pushState', updateHistory.bind(null, 'push'))
-      bus.prependListener('replaceState', updateHistory.bind(null, 'replace'))
+Choo.prototype.use = function (cb) {
+  assert.equal(typeof cb, 'function', 'choo.use: cb should be type function')
+  var endTiming = nanotiming('choo.use')
+  cb(this.state, this.emitter, this)
+  endTiming(this._trace.bind(this))
+}
 
-      if (opts.href !== false) {
-        nanohref(function (location) {
-          var href = location.href
-          var currHref = window.location.href
-          if (href === currHref) return
-          bus.emit('pushState', href)
-        })
-      }
+Choo.prototype.start = function () {
+  assert.equal(typeof window, 'object', 'choo.start: window was not found. .start() must be called in a browser, use .toString() if running in Node')
+
+  var self = this
+
+  if (this._historyEnabled) {
+    window.onpopstate = function () {
+      self.emitter.emit('pushState')
     }
 
-    function updateHistory (mode, href) {
-      if (href) window.history[mode + 'State']({}, null, href)
-      bus.emit('render')
-      setTimeout(function () {
-        scrollIntoView()
-      }, 0)
+    this.emitter.prependListener('pushState', function (href) {
+      if (href) window.history.pushState({}, null, href)
+      self.emitter.emit('render')
+      setTimeout(scrollIntoView, 0)
+    })
+
+    this.emitter.prependListener('replaceState', function (href) {
+      if (href) window.history.replaceState({}, null, href)
+      self.emitter.emit('render')
+      setTimeout(scrollIntoView, 0)
+    })
+
+    if (self._hrefEnabled) {
+      nanohref(function (location) {
+        var href = location.href
+        var currHref = window.location.href
+        if (href === currHref) return
+        self.emitter.emit('pushState', href)
+      })
     }
-
-    rerender = nanoraf(function () {
-      if (hasPerformance && timingEnabled) {
-        window.performance.mark('choo:renderStart')
-      }
-      var newTree = router(createLocation())
-      tree = nanomorph(tree, newTree)
-      assert.notEqual(tree, newTree, 'choo.start: a different node type was returned as the root node on a rerender. Make sure that the root node is always the same type to prevent the application from being unmounted.')
-      if (hasPerformance && timingEnabled) {
-        window.performance.mark('choo:renderEnd')
-        window.performance.measure('choo:render', 'choo:renderStart', 'choo:renderEnd')
-      }
-    })
-
-    bus.prependListener('render', rerender)
-
-    documentReady(function () {
-      bus.emit('DOMContentLoaded')
-    })
-
-    tree = router(createLocation())
-
-    return tree
   }
 
-  function emit (eventName, data) {
-    bus.emit(eventName, data)
-  }
+  var location = this._createLocation()
+  this._tree = this.router(location)
+  assert.ok(this._tree, 'choo.start: no valid DOM node returned for location ' + location)
 
-  function mount (selector) {
-    var newTree = start()
-    documentReady(function () {
-      var root = document.querySelector(selector)
-      assert.ok(root, 'choo.mount: could not query selector: ' + selector)
-      nanomount(root, newTree)
-      tree = root
-    })
-  }
+  this.emitter.prependListener('render', nanoraf(function () {
+    var renderTiming = nanotiming('choo.render')
+    var newTree = self.router(self._createLocation())
 
-  function toString (location, _state) {
-    state = _state || {}
-    var html = router(location)
-    return html.toString()
-  }
+    var morphTiming = nanotiming('choo.morph')
+    self._tree = nanomorph(self._tree, newTree)
+    assert.ok(self._tree, 'choo.render: no valid DOM node returned for location ' + location)
+    morphTiming(self._trace.bind(self))
+
+    renderTiming(self._trace.bind(self))
+  }))
+
+  documentReady(function () {
+    self.emitter.emit('DOMContentLoaded')
+  })
+
+  return this._tree
+}
+
+Choo.prototype.mount = function mount (selector) {
+  assert.equal(typeof window, 'object', 'choo.mount: window was not found. .mount() must be called in a browser, use .toString() if running in Node')
+  assert.equal(typeof selector, 'string', 'choo.mount: selector should be type string')
+
+  var self = this
+
+  documentReady(function () {
+    var renderTiming = nanotiming('choo.render')
+    var newTree = self.start()
+
+    var root = document.querySelector(selector)
+    assert.ok(root, 'choo.mount: could not query selector: ' + selector)
+
+    var morphTiming = nanotiming('choo.morph')
+    var resultTree = nanomorph(root, newTree)
+    morphTiming(self._trace.bind(self))
+
+    assert.equal(resultTree, root, 'choo.mount: The target node <' +
+      resultTree.nodeName.toLowerCase() + '> is not the same type as the new node <' +
+      newTree.nodeName.toLowerCase() + '>.')
+
+    self._tree = root
+    renderTiming(self._trace.bind(self))
+  })
+}
+
+Choo.prototype.toString = function (location, state) {
+  this.state = state || {}
+
+  assert.equal(typeof location, 'string', 'choo.toString: location should be type string')
+  assert.equal(typeof this.state, 'object', 'choo.toString: state should be type object')
+
+  var html = this.router(location)
+  assert.ok(html, 'choo.toString: no valid value returned for the route ' + location)
+  return html.toString()
+}
+
+Choo.prototype._createLocation = function () {
+  var pathname = window.location.pathname.replace(/\/$/, '')
+  var hash = window.location.hash.replace(/^#/, '/')
+  return pathname + hash
+}
+
+Choo.prototype._trace = function (timing, name) {
+  if (timing) timing.__name = name
+  this.emitter.emit('trace', timing)
 }
 
 function scrollIntoView () {
@@ -129,10 +167,4 @@ function scrollIntoView () {
       if (el) el.scrollIntoView(true)
     } catch (e) {}
   }
-}
-
-function createLocation () {
-  var pathname = window.location.pathname.replace(/\/$/, '')
-  var hash = window.location.hash.replace(/^#/, '/')
-  return pathname + hash
 }
