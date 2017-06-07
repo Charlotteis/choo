10c32c8583c28d53251086bda3c1d0690a4ea477
Yoshua Wuyts <yoshuawuyts@gmail.com>
2017-06-08 01:52:23 +0200

    expose route on state

--- index.js
+++ index.js
@@ -39,6 +39,7 @@ Choo.prototype.route = function (route, handler) {
   this.router.on(route, function (params) {
     return function () {
       self.state.params = params
+      self.state.route = route
       var routeTiming = nanotiming("choo.route('" + route + "')")
       var res = handler(self.state, function (eventName, data) {
         self.emitter.emit(eventName, data)
